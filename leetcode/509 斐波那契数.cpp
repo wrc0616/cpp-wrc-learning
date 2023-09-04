99c64e699672edcf6b7190bf1dca7d1adda8ede9
wrc0616 <119781908+wrc0616@users.noreply.github.com>
2023-09-04 21:04:48 +0800

    Update 509 斐波那契数.cpp

--- leetcode/509 斐波那契数.cpp
+++ leetcode/509 斐波那契数.cpp
@@ -18,3 +18,20 @@ public:
         return vale;
     }
 };
+//动态规划
+class Solution {
+public:
+    int fib(int n) {
+        int left=0;
+        int right = 1;
+        int sum = left+right;
+        //n为2开始
+        for(int i =2;i<n;i++)
+        {
+            left = right;
+            right =sum;
+            sum = left+right;  
+        }
+        return sum;
+    }
+};
