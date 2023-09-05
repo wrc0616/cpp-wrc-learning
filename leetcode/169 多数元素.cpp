85651e64eaa8f7a0619ad61049c5bce600ffdccc
wrc0616 <119781908+wrc0616@users.noreply.github.com>
2023-09-05 17:41:02 +0800

    Create 169 多数元素.cpp

---
+++ leetcode/169 多数元素.cpp
@@ -0,0 +1,49 @@
+//分治法
+class Solution {
+public:
+    //注意这里的left和right不能使用引用，会一直改变。
+    int getmajority(vector<int> &nums,int left,int right)
+    {
+        //首先判断最后一个是不是一个值，不可以在分了
+        if(left == right)
+        {
+            return nums[left];
+        }
+        //每次取中间值
+        int mid = (left+right)/2;
+        //进入递归，左边最多的值输出
+        int leftmajority = getmajority(nums,left,mid);
+        //进入递归，右边最多的值输出
+        int rightmajority = getmajority(nums,mid+1,right);
+        //如果左边的值等于右边的值，直接返回相等的这个值
+        if(leftmajority==rightmajority)
+        {
+            return leftmajority;
+        }
+        int leftcount=0;
+        int rightcount = 0;
+        //如果不相等，判断那个值出现的多，就返回哪个值
+        for(int i=left;i<=right;i++)
+        {
+            if(nums[i]==leftmajority)
+            {
+                leftcount++;
+            }
+            else if(nums[i]==rightmajority)
+            {
+                rightcount++;
+            }
+        }
+        if(rightcount>leftcount)
+        {
+            return rightmajority;
+        }
+        else
+        {
+            return leftmajority;
+        }
+    }
+    int majorityElement(vector<int>& nums) {
+        return getmajority(nums,0,nums.size()-1);
+    }
+};
